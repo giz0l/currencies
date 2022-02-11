af402f4ee68597c6fdb999bd5a864f39a695dcf4
giz0l <45897377+giz0l@users.noreply.github.com>
2022-02-11 23:57:19 +0100

    Create NBP service and command

---
+++ app/Services/NBP.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Services;
+use GuzzleHttp\Client;
+
+class NBP
+{    
+    public function getCurrency($table)
+    {
+        $client = new Client();
+        $response = $client->request('GET', 'http://api.nbp.pl/api/exchangerates/tables/'.$table, [
+            'headers' => [
+                'accept' => 'application/json',
+            ]
+        ]);
+        if ($response->getStatusCode() == 200)
+        {
+            $content = json_decode($response->getBody());
+            return $content;
+        }
+    }
+}
